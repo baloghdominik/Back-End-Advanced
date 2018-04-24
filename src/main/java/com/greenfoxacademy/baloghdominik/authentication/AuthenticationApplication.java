79a380b57a67b5cb3b8750ccde62dd7176986570
baloghdominik <balogh0dominik@gmail.com>
2018-04-24 10:28:11 +0200

    Created the project base

---
+++ src/main/java/com/greenfoxacademy/baloghdominik/authentication/AuthenticationApplication.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.baloghdominik.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by baloghdominik on 2018. 04. 24..
+ */
+
+@SpringBootApplication
+public class AuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+}
---
+++ src/main/java/com/greenfoxacademy/baloghdominik/authentication/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.baloghdominik.authentication.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by baloghdominik on 2018. 04. 24..
+ */
+
+@Controller
+public class MainController {
+
+}
